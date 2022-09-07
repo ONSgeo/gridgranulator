21aaca32c50a55226a59689f2e0b224c8718f057
Kerr <David.Kerr@ons.gov.uk>
2022-09-07 07:17:20 +0100

    Make setup.py and copy wheel dependencies (these may be removed later)

---
+++ setup.py
@@ -0,0 +1,31 @@
+from pathlib import Path
+from setuptools import find_packages
+from setuptools import setup
+
+DEPS = Path(__file__).resolve().parent.joinpath('geo_wheels')
+
+rtree = str(DEPS.joinpath('Rtree-0.9.7-cp38-cp38-win_amd64.whl'))
+gdal = str(DEPS.joinpath('GDAL-3.2.2-cp38-cp38-win_amd64.whl'))
+fiona = str(DEPS.joinpath('Fiona-1.8.18-cp38-cp38-win_amd64.whl'))
+pyproj = str(DEPS.joinpath('pyproj-3.0.1-cp38-cp38-win_amd64.whl'))
+shapely = str(DEPS.joinpath('Shapely-1.8a3-cp38-cp38-win_amd64.whl'))
+
+
+
+setup(
+    name='gridgran',
+    version='1.0.0',
+    description='Package to disseminate census point data using hierarchical grids with the aim of showing as much spatial detail as possible whilst not breaking disclosure rules',
+    authoer='David Kerr',
+    author_email='david.kerr@ons.gov.uk',
+    url='https://github.com/ONSgeo/gridgranulator',
+    install_requires=[
+        f"rtree @ file://localhost/{rtree}",
+        f"gdal @ file://localhost/{gdal}",
+        f"fiona @ file://localhost/{fiona}",
+        f"pyproj @ file://localhost/{pyproj}",
+        f"shapely @ file://localhost/{shapely}",
+        ],
+    packages=find_packages(exclude=('tests*')),
+
+)
---
+++ tests/test_hello.py
@@ -0,0 +1,4 @@
+from gridgran import hello
+
+def test_hello_function():
+    assert hello() == 'Good morning'
